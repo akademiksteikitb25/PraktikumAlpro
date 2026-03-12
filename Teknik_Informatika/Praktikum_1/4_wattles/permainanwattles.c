2e0db44fde74463246a79667bf0518c6634d5a50
SByte <stormybytes@gmail.com>
2026-03-12 09:21:34 +0700

    avoid defining new functions in IF prak1

--- Teknik_Informatika/Praktikum_1/4_wattles/permainanwattles.c
+++ Teknik_Informatika/Praktikum_1/4_wattles/permainanwattles.c
@@ -1,27 +1,27 @@
 /* Dan - 13525052 */
 #include <stdio.h>
 
-int first(int x) {
-	while (x >= 10) {
-		x /= 10;
-	}
-	return x;
-}
-
-int last(int x) {
-	return x % 10;
-}
-
 int main() {
 	int d, m;
 	scanf("%d %d", &d, &m);
 
 	while (d >= 0 && m >= 0 && d != m) {
-		int nd = d - (first(m) + last(m));
-		int nm = m - (first(d) + last(d));
-		d = nd;
-		m = nm;
+		int nd = m;
+		while (nd >= 10) {
+			nd = nd / 10;
+		};
+		nd = nd + (m % 10);
+
+		int nm = d;
+		while (nm >= 10) {
+			nm = nm / 10;
+		};
+		nm = nm + (d % 10);
+
+		d = d - nd;
+		m = m - nm;
 	}
+
 	if (d == m || (d < 0 && m < 0)) {
 		printf("Wattles\n");
 	} else if (d >= 0) {
