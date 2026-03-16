134fdc8882a84251a064bd8c82a97f08913f3d48
akademiksteikitb25 <akademiksteikitb25@gmail.com>
2026-03-16 09:31:21 +0700

    REVISI JAWABAN SOAL PRAKTIKUM 1 STI

--- Sistem_dan_Teknologi_Informasi/Praktikum1/Mebel/mebel.c
+++ Sistem_dan_Teknologi_Informasi/Praktikum1/Mebel/mebel.c
@@ -1,26 +1,15 @@
+// Program Mebel 
+// Program untuk menggambar pola huruf "M"
+
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    
-    if (n < 3 || n > 100) {
+    int N;
+    scanf("%d", &N);
+
+    int lebar = (2 * N) - 1;
+    if (N < 3 || N > 100) {
         printf("Masukan tidak valid\n");
-        return 0;
     }
-    
-    int width = 2 * n - 1;
-    
-    for (int i = 0; i < n; i++) { 
-        for (int j = 0; j < width; j++) {
-            if (j == 0 || j == i || j == width - i || j == width - 1) {
-                printf("M");
-            } else {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
-    
     return 0;
 }
