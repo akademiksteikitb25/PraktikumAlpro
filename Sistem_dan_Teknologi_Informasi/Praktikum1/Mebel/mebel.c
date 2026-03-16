ccc8e32fcd2df9f970c79e61b2c2c5f3e56f24d1
akademiksteikitb25 <akademiksteikitb25@gmail.com>
2026-03-16 09:32:37 +0700

    Update mebel.c

--- Sistem_dan_Teknologi_Informasi/Praktikum1/Mebel/mebel.c
+++ Sistem_dan_Teknologi_Informasi/Praktikum1/Mebel/mebel.c
@@ -7,9 +7,25 @@ int main() {
     int N;
     scanf("%d", &N);
 
-    int lebar = (2 * N) - 1;
     if (N < 3 || N > 100) {
         printf("Masukan tidak valid\n");
+        return 0;
     }
+
+    int lebar = 2 * N - 1;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < lebar; j++) {
+
+            if (j == 0 || j == lebar - 1 || j == i || j == lebar - 1 - i) {
+                printf("M");
+            } else {
+                printf(" ");
+            }
+
+        }
+        printf("\n");
+    }
+
     return 0;
 }
